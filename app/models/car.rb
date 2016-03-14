4e213bd11720187bef7ab779fe6022234ea2f7c1
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2016-03-15 01:47:19 +0200

    Update Unicorn socket path

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,4 +1,14 @@
 class Car < ActiveRecord::Base
   belongs_to :user
   has_many :histories, dependent: :destroy
+
+  private
+
+  def validate_production_history
+    # can have only 1 production history
+  end
+
+  def validate_purchase_history
+    # can have only 1 purchase history
+  end
 end
