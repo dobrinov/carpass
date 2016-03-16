b49bc7e4bc85469ecec4eb5cffa895a0e6f5231a
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2016-03-16 21:50:40 +0200

    Fix wrong validation

--- app/models/history.rb
+++ app/models/history.rb
@@ -2,7 +2,7 @@ class History < ActiveRecord::Base
   belongs_to :car
 
   validates :mileage, presence: true,
-                      numericality: { only_integer: true, greater_than: 0 }
+                      numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :details, presence: true
 
   validate :validate_mileage_value
@@ -10,7 +10,7 @@ class History < ActiveRecord::Base
   private
 
   def validate_mileage_value
-    if mileage && mileage < car.histories.last.mileage
+    if mileage && car.histories.last && mileage < car.histories.last.mileage
       errors.add(:mileage, I18n.t('activemodel.errors.models.history.attributes.mileage.lower_than_last_recorded'))
     end
   end
