139e1f1a9e2de4afc67f857c8e241004106332ba
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2016-03-16 18:12:50 +0200

    Implement history validations

--- app/models/history.rb
+++ app/models/history.rb
@@ -3,5 +3,15 @@ class History < ActiveRecord::Base
 
   validates :mileage, presence: true,
                       numericality: { only_integer: true, greater_than: 0 }
-  validates :created_at, presence: true
+  validates :details, presence: true
+
+  validate :validate_mileage_value
+
+  private
+
+  def validate_mileage_value
+    if mileage && mileage < car.histories.last.mileage
+      errors.add(:mileage, I18n.t('activemodel.errors.models.history.attributes.mileage.lower_than_last_recorded'))
+    end
+  end
 end
