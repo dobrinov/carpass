f405e0fe271e2cf9aa51fa0a2bf86d6ae7aee358
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2016-03-16 17:46:45 +0200

    Accept only integer mileage

--- app/models/history.rb
+++ app/models/history.rb
@@ -1,6 +1,7 @@
 class History < ActiveRecord::Base
   belongs_to :car
 
-  validates :mileage,    presence: true
+  validates :mileage, presence: true,
+                      numericality: { only_integer: true, greater_than: 0 }
   validates :created_at, presence: true
 end
