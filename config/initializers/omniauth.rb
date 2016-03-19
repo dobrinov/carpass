1574ceff3d982ebf04ddb13c0cb78002500a487a
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2016-03-19 12:30:52 +0200

    Use latest version of the Facebook API and fetch all fields

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,13 +4,17 @@ Rails.application.config.middleware.use OmniAuth::Builder do
     facebook_key    = ENV['FACEBOOK_KEY']
     facebook_secret = ENV['FACEBOOK_SECRET']
   else
-    facebook_key    = 521198641395119
-    facebook_secret = 'a59f0a3c0e9f16f87f066e097a846966'
+    facebook_key    = 211397485887858
+    facebook_secret = '86a1175ebf2a7b3ac7a0fc0002a6a091'
   end
 
-  provider :facebook, facebook_key, facebook_secret, scope: 'email', display: 'page'
-  # client_options: {
-  #   site: 'https://graph.facebook.com/v2.3',
-  #   authorize_url: "https://www.facebook.com/v2.3/dialog/oauth"
-  # }
+  provider :facebook, facebook_key, facebook_secret,
+  scope: 'public_profile,email',
+  display: 'page',
+  info_fields: 'id,email,location,first_name,last_name,gender,picture',
+  :client_options => {
+    :site => 'https://graph.facebook.com/v2.5',
+    :authorize_url => "https://www.facebook.com/v2.5/dialog/oauth"
+  },
+  token_params: { parse: :json }
 end
