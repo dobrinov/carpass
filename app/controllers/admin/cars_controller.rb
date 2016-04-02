29ceea511f29756270eebc198a5defc611ae111a
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2016-04-02 12:30:53 +0300

    Order records in admin by date of creation

--- app/controllers/admin/cars_controller.rb
+++ app/controllers/admin/cars_controller.rb
@@ -2,7 +2,7 @@ module Admin
   class CarsController < BaseController
     def index
       @user = User.find(params[:user_id])
-      @cars = @user.cars
+      @cars = @user.cars.order(created_at: :desc)
     end
   end
 end
