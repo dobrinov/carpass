6e74ab4e4f10bfe3c3f564cd47dce3e02f65efb7
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2016-04-02 13:38:32 +0300

    Order histories correctly

--- app/controllers/admin/histories_controller.rb
+++ app/controllers/admin/histories_controller.rb
@@ -2,7 +2,7 @@ module Admin
   class HistoriesController < BaseController
     def index
       @car = Car.find(params[:car_id])
-      @histories = @car.histories
+      @histories = @car.histories.order(created_at: :desc)
     end
   end
 end
