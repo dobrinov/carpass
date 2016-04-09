f364c2495a5e57aca9dcae80203c4ba1cea84b80
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2016-04-10 01:29:57 +0300

    Make purchase and production editable

--- app/controllers/purchase_histories_controller.rb
+++ app/controllers/purchase_histories_controller.rb
@@ -2,13 +2,21 @@ class PurchaseHistoriesController < HistoriesController
   def show
   end
 
+  def update
+    if @history.update_attributes(history_params)
+      redirect_to back_or_default
+    else
+      render :edit
+    end
+  end
+
   private
 
   def history_params
-    # if params[:full_insurance_history].present?
-    #   params[:full_insurance_history].permit(:mileage, :valid_until, :cost, :details)
-    # else
-    #   {}
-    # end
+    if params[:purchase_history].present?
+      params[:purchase_history].permit(:happened_at, :mileage)
+    else
+      {}
+    end
   end
 end
