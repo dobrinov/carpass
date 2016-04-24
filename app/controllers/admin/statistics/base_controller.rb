b758a575d8a94784b7a63ea7936b14cfb461c6d9
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2016-04-25 02:04:52 +0300

    Enhance statistics

--- app/controllers/admin/statistics/base_controller.rb
+++ app/controllers/admin/statistics/base_controller.rb
@@ -2,6 +2,13 @@ module Admin
   module Statistics
     class BaseController < Admin::BaseController
       def overview
+        @all_users_count     = User.count
+        @active_users_count  = History.where("type NOT IN('ProductionHistory', 'PurchaseHistory')")
+                                      .includes(car: [:user])
+                                      .map(&:car).map(&:user)
+                                      .uniq.count
+        @all_cars_count      = Car.count
+        @all_histories_count = History.count
       end
     end
   end
