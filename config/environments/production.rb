c9739155241d645ef078bb7c5990128bc508d713
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2016-05-03 11:13:54 +0300

    Remove passwords from source contro. Oops.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,8 +83,8 @@ Rails.application.configure do
     address:              'smtp.sendgrid.net',
     port:                 587,
     domain:               'autobook.bg',
-    user_name:            'didko',
-    password:             'Revival123',
+    user_name:            ENV['SENDGRID_USERNAME'],
+    password:             ENV['SENDGRID_PASSWORD'],
     authentication:       :plain,
     enable_starttls_auto: true
   }
