fefeab357f1810aa11a70c3fcd57ebb9d39d225c
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2016-05-08 19:13:50 +0300

    Change cron execution time and log file location

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,6 +2,6 @@ env :PATH, ENV['PATH']
 env :SENDGRID_USERNAME, ENV['SENDGRID_USERNAME']
 env :SENDGRID_PASSWORD, ENV['SENDGRID_PASSWORD']
 
-every 1.day, at: '7:11pm' do
-  rake "histories:notify_expiration", output: 'cron_history_expiration_notifier.log'
+every 1.day, at: '12:30pm' do
+  rake "histories:notify_expiration", output: 'log/cron/history_expiration_notifier.log'
 end
