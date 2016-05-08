15b771a3b9cda1a732d6c331a0854b642f972d5a
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2016-05-08 19:08:48 +0300

    Expose sendgrid env vars

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,7 @@
 env :PATH, ENV['PATH']
+env :SENDGRID_USERNAME, ENV['SENDGRID_USERNAME']
+env :SENDGRID_PASSWORD, ENV['SENDGRID_PASSWORD']
 
-every 1.day, at: '2:45pm' do
+every 1.day, at: '7:11pm' do
   rake "histories:notify_expiration", output: 'cron_history_expiration_notifier.log'
 end
