d861a1c44691edec992c7add1d8d9f13f000e4f0
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2016-05-10 09:32:09 +0300

    Remove whenever logging

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,5 +3,5 @@ env :SENDGRID_USERNAME, ENV['SENDGRID_USERNAME']
 env :SENDGRID_PASSWORD, ENV['SENDGRID_PASSWORD']
 
 every 1.day, at: '12:30pm' do
-  rake "histories:notify_expiration", output: 'log/cron/history_expiration_notifier.log'
+  rake "histories:notify_expiration"
 end
