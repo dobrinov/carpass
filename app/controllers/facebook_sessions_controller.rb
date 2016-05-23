a956d32b04dcb4968714ec338c8ea52ca61b1188
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2016-05-23 22:30:42 +0300

    Add Facebook signup completion pixel

--- app/controllers/facebook_sessions_controller.rb
+++ app/controllers/facebook_sessions_controller.rb
@@ -1,6 +1,11 @@
 class FacebookSessionsController < ApplicationController
   def create
     user = FacebookUser.from_omniauth(env["omniauth.auth"])
+
+    if user_created_last_minute?(user)
+      flash[:facebook_tracking] = 'signup'
+    end
+
     log_in(user)
     redirect_to back_or_default
   end
@@ -9,5 +14,11 @@ class FacebookSessionsController < ApplicationController
 
   def default_path
     cars_path
+  end
+
+  private
+
+  def user_created_last_minute?(user)
+    user.created_at > Time.current - 1.minute
   end
 end
