75af24651bfa53abb9d8a243f9c674fe627bb0f0
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2016-07-23 13:54:42 +0300

    Add facebook key and secret to global config

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,14 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
 
-  if Rails.env.production?
-    facebook_key    = ENV['FACEBOOK_KEY']
-    facebook_secret = ENV['FACEBOOK_SECRET']
-  else
-    facebook_key    = 211397485887858
-    facebook_secret = '86a1175ebf2a7b3ac7a0fc0002a6a091'
-  end
-
-  provider :facebook, facebook_key, facebook_secret,
+  provider :facebook, AppConfig.facebook['key'], AppConfig.facebook['secret'],
   scope: 'public_profile,email',
   display: 'page',
   info_fields: 'id,email,location,first_name,last_name,gender,picture',
