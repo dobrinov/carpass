f2194d7426e782e92cfdecfbdc5bfb317c949c44
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2016-09-05 15:54:52 +0300

    Implement guest user registration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160720215244) do
+ActiveRecord::Schema.define(version: 20160904104138) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,6 +72,7 @@ ActiveRecord::Schema.define(version: 20160720215244) do
     t.datetime "updated_at",             null: false
     t.string   "password_reset_token"
     t.datetime "password_reset_sent_at"
+    t.boolean  "guest"
   end
 
 end
