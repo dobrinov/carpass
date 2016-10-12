a097c84feed04bb07fa0a867b432dcc26c3e1e25
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2016-10-12 22:04:01 +0300

    Make it possible to specify history validity in the past

--- app/models/history.rb
+++ app/models/history.rb
@@ -5,8 +5,8 @@ class History < ActiveRecord::Base
   validates :mileage, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, allow_blank: true
 
   validates :happened_at, presence: true
-
   validates :cost, numericality: { allow_blank: true, greater_than_or_equal_to: 0 }
+  validate :valid_until_is_after_happened_at
 
   def notify_about_expiration?
     expiration_notification_class.present?
@@ -18,5 +18,13 @@ class History < ActiveRecord::Base
 
   def notify_expiration
     # Do nothing
+  end
+
+  private
+
+  def valid_until_is_after_happened_at
+    if valid_until.present? && valid_until < happened_at
+      errors.add(:valid_until, I18n.t('activerecord.errors.models.history.attributes.valid_until.in_the_past'))
+    end
   end
 end
