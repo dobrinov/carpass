119e2774faa407100607715862dd824ac627974e
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2018-04-12 01:02:22 +0300

    Update Omniauth and used Facebook API

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -5,8 +5,8 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   display: 'page',
   info_fields: 'id,email,location,first_name,last_name,gender,picture',
   :client_options => {
-    :site => 'https://graph.facebook.com/v2.5',
-    :authorize_url => "https://www.facebook.com/v2.5/dialog/oauth"
+    :site => 'https://graph.facebook.com/v2.12',
+    :authorize_url => "https://www.facebook.com/v2.12/dialog/oauth"
   },
   token_params: { parse: :json }
 end
