932c7172d8d046aa16738aabb8f722be6da029b9
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2020-05-03 23:34:15 +0300

    Improve seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,11 @@
 def create_user(email)
   password = '1'
-  user = User.new email: 'homer@example.com', password: password, password_confirmation: password
+  user = User.new email: 'homer@example.com',
+                  password: password,
+                  password_confirmation: password,
+                  last_login_at: Time.current
+
+  user.build_setting
   user.save! validate: false
   user
 end
@@ -8,6 +13,4 @@ end
 homer = create_user 'homer@example.com'
 
 bmw = Brand.create! name: 'BMW'
-
-car = Car.crerate! user: homer, brand: 'BMW', model: '325i'
-
+car = Car.create! user: homer, brand: bmw, model: '325i', plate: 'CB1234XX'
